3529b0e9cdb83cd528118a2d525c1277efb891fc
sharmainemanuel <manuelsharmaine@gmail.com>
2016-04-28 09:38:43 +0800

    2.49

---
+++ js/gfa.js
@@ -0,0 +1,26 @@
+			$(document).ready(function(){
+		$.ajax
+({
+
+  type: "GET",
+  url: "http://m.weather.gov.ph/agaptest/gfa.php",
+  async: false,
+
+  success: function (result2){
+	setTimeout(function(){
+		$('#imgLoader').fadeOut();
+
+ 	var region = result2.result['26f22ef9-c7af-4474-b3a1-85dd44a74ba6'].info.parameter.value;
+ 	var area = result2.result['26f22ef9-c7af-4474-b3a1-85dd44a74ba6'].info.area.areaDesc;
+ 	var headline = result2.result['26f22ef9-c7af-4474-b3a1-85dd44a74ba6'].info.headline;
+ 	var description = result2.result['26f22ef9-c7af-4474-b3a1-85dd44a74ba6'].info.description;
+	
+	$( ".inner" ).html("<p><span>Region: </span><br>" +region+"</p><p><span>Area: </span><br>" +area+"</p><p><span>Headline: </span><br>" +headline+"</p><p><span>Description: </span><br>" +description+"</p>");
+	 }, 1000);
+	},  error: function (request, textStatus, errorThrown) {
+        //console.log(request.responseText);
+        console.log(textStatus);
+        console.log(errorThrown);
+    }
+});
+});
