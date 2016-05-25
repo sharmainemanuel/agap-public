f96c9ed4576300a4fb5556904515325fe253da1e
sharmainemanuel <manuelsharmaine@gmail.com>
2016-05-25 09:59:50 +0800

    2.55

--- js/warnings_shipping.js
+++ js/warnings_shipping.js
@@ -1,1 +1,52 @@
-$(document).ready(function(){$.ajax({type:"GET",url:"http://m.weather.gov.ph/agaptest/iws.php",async:!1,success:function(i){setTimeout(function(){$("#imgLoader").fadeOut(),localStorage.setItem("warning_category",i.result[0].warning_category),localStorage.setItem("warning_number",i.result[0].warning_number),localStorage.setItem("ty_type",i.result[0].tc_type);var t=(i.result[0].date,localStorage.getItem("warning_category")),l=localStorage.getItem("warning_number"),e=localStorage.getItem("ty_type"),a=i.result[0].int_tc_name,s=i.result[0].tc_number,r=i.result[0].includes_radar,n=i.result[0].location_latlng,c=i.result[0].fcst,o=i.result[0].fcst_value,u=i.result[0].sea_condition,d=i.result[0].tc_radius,g=i.result[0].extent;$(".inner").append("<ul data-role=listview data-inset=false data-icon=false data-divider-theme=b class=ui-listview> <li class='ui-li-static ui-body-inherit ui-first-child'> <span>Warning Category:</span> "+t+" </li> <li class='ui-li-static ui-body-inherit'> Warning Number: "+l+" </li></li> <li class='ui-li-static ui-body-inherit'> Tropical Type: "+e+" </li></li> <li class='ui-li-static ui-body-inherit'> Tropical International Name: "+a+" </li></li> <li class='ui-li-static ui-body-inherit'> Tropical Number: "+s+" </li></li> <li class='ui-li-static ui-body-inherit'> Includes Radar: "+r+" </li></li> <li class='ui-li-static ui-body-inherit'> Location(lot,lang): "+n+" </li></li> <li class='ui-li-static ui-body-inherit'> Forecast: "+c+" </li></li> <li class='ui-li-static ui-body-inherit'> Forecast Value: "+o+" </li></li> <li class='ui-li-static ui-body-inherit'> Sea Condition: "+u+" </li></li> <li class='ui-li-static ui-body-inherit'> Tropical Radius: "+d+" </li></li> <li class='ui-li-static ui-body-inherit'> Extent: "+g+" </li></ul>")},1e3)},error:function(i,t,l){console.log(i.responseText),console.log(t),console.log(l)}})});
+
+$(document).ready(function(){
+$.ajax
+({
+
+  type: "GET",
+  url: "http://m.weather.gov.ph/agaptest/iws.php",
+  async: false,
+
+  success: function (result2){
+	setTimeout(function(){
+		$('#imgLoader').fadeOut();
+	localStorage.setItem("warning_category", result2.result[0].warning_category);
+	localStorage.setItem("warning_number", result2.result[0].warning_number);
+	localStorage.setItem("ty_type", result2.result[0].tc_type);
+
+
+
+  	var date = result2.result[0].date;
+ 	var warning_category = localStorage.getItem("warning_category");
+ 	var warning_number = localStorage.getItem("warning_number");
+ 	var ty_type = localStorage.getItem("ty_type");
+	var int_tc_name = result2.result[0].int_tc_name;
+ 	var tc_number = result2.result[0].tc_number;
+	var includes_radar = result2.result[0].includes_radar;
+	var location_latlng = result2.result[0].location_latlng;
+	var fcst = result2.result[0].fcst;
+	var fcst_value = result2.result[0].fcst_value;
+	var sea_condition = result2.result[0].sea_condition;
+	var tc_radius = result2.result[0].tc_radius;
+	var extent = result2.result[0].extent;
+
+
+	if (int_tc_name == "" || int_tc_name == undefined)
+		int_tc_name = "NONE";
+	
+	if (tc_number == "" || tc_number == undefined)
+		tc_number = "NONE";	
+	
+	if (includes_radar == "" || includes_radar == undefined)
+		includes_radar = "NONE";
+
+	$( ".inner" ).append("<ul data-role=listview data-inset=false data-icon=false data-divider-theme=b class=ui-listview> <li class='ui-li-static ui-body-inherit ui-first-child'> <span>Warning Category:</span> "+warning_category+" </li> <li class='ui-li-static ui-body-inherit'> Warning Number: "+warning_number+" </li></li> <li class='ui-li-static ui-body-inherit'> Tropical Type: "+ty_type+" </li></li> <li class='ui-li-static ui-body-inherit'> Tropical International Name: "+int_tc_name+" </li></li> <li class='ui-li-static ui-body-inherit'> Tropical Number: "+tc_number+" </li></li> <li class='ui-li-static ui-body-inherit'> Includes Radar: "+includes_radar+" </li></li> <li class='ui-li-static ui-body-inherit'> Location(lot,lang): "+location_latlng+" </li></li> <li class='ui-li-static ui-body-inherit'> Forecast: "+fcst+" </li></li> <li class='ui-li-static ui-body-inherit'> Forecast Value: "+fcst_value+" </li></li> <li class='ui-li-static ui-body-inherit'> Sea Condition: "+sea_condition+" </li></li> <li class='ui-li-static ui-body-inherit'> Tropical Radius: "+tc_radius+" </li></li> <li class='ui-li-static ui-body-inherit'> Extent: "+extent+" </li></ul>");
+ }, 1000);
+	},  error: function (request, textStatus, errorThrown) {
+        console.log(request.responseText);
+        console.log(textStatus);
+        console.log(errorThrown);
+    }
+});
+});
+	
--- js/weekly.js
+++ js/weekly.js
@@ -1,1 +1,6 @@
-$(document).ready(function(){$.ajax({type:"GET",url:"http://m.weather.gov.ph/agaptest/weekly_outlook.php",async:!1,success:function(t){setTimeout(function(){$("#imgLoader").fadeOut();var e=t.result[0].issuedtime,s=t.result[0].outlook1;$(".inner").html("<h4><strong>Issued Date: </strong>"+e+"</h4><p>"+s+"</p>")},1e3)}})});
+$(document).ready(function(){$.ajax({type:"GET",url:"http://m.weather.gov.ph/agaptest/weekly_outlook.php",async:!1,success:function(t){setTimeout(function(){
+	
+	$("#imgLoader").fadeOut();var e=t.result[0].issuedtime,s=t.result[0].outlook1;
+	 if(s.includes("Â"))
+    s = s.replace("Â", "");
+	$(".inner").html("<p>"+s+"</p>")},1e3)}})});
